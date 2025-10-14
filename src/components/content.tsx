d48a6bc343a6af92a2570fafacf7167cc73d24cf
jjdavenport <jordanjamesdavenport@gmail.com>
2025-10-14 18:20:14 +0100

    add image loading state

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import image from "../assets/image-qr-code.png";
 
 export const Footer = () => {
@@ -43,12 +44,14 @@ export const Container = ({ children }: { children: React.ReactNode }) => {
 };
 
 export const Main = () => {
+  const [loaded, setLoaded] = useState(false);
   return (
     <>
       <main className="flex h-[31.1875rem] w-80 flex-col justify-between gap-6 rounded-[1.25rem] bg-white px-4 pt-4 pb-20 text-center shadow-[0_25px_25px_0_rgba(0,0,0,0.0477)]">
         <img
+          onLoad={() => setLoaded(true)}
           src={image}
-          className="h-[18rem] w-[18rem] rounded-[0.625rem] object-contain"
+          className={`h-[18rem] w-[18rem] rounded-[0.625rem] object-contain transition-opacity duration-500 ${loaded ? "opacity-100" : "opacity-0"}`}
           alt="qr code"
         />
         <div className="flex h-[8.1875rem] w-72 flex-col items-center gap-4">
